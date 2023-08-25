36b19dea7c80e0b04a28eae9869f9bd408238326
Andrii <jaworovcki@gmail.com>
2023-08-25 09:10:43 +0300

    Create data model for Federal State of the Austria

---
+++ Models/FederalState.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoronaStatsAustria.Models
+{
+    public class FederalState
+    {
+        public int Id { get; set; }
+
+        [MaxLength(250)]
+        [Required]
+        public string Name { get; set; }
+    }
+}
